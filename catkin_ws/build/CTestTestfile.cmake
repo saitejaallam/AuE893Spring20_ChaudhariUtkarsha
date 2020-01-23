ea8ecdb9faaa45668a2cd7705925ddf0c48ade7f
Chaudhari Utkarsha <uchaudh@clemson.edu>
2020-01-23 13:51:41 -0500

    comitting 01/23

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/utkarsha/AuE893Spring20_ChaudhariUtkarsha/catkin_ws/src
+# Build directory: /home/utkarsha/AuE893Spring20_ChaudhariUtkarsha/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
